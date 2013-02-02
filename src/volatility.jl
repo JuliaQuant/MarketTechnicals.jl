5a994837f16f805a8760430506dc75f2cdbec3ca
milktrader <milktrader@gmail.com>
2013-02-02 11:31:11 -0500

    std window equal to ma length in bollinger_band

--- src/volatility.jl
+++ src/volatility.jl
@@ -2,12 +2,13 @@ function bollinger_bands(df::DataFrame, col::String, ma::Int, width::Float64)
 
   df = copy(df) #preserve the original DataFrame
   ma_col = strcat("mean_", ma)
+  sd_col = strcat("std_", ma)
   df_ma  = moving!(df, col, mean, ma)
   df_new = df_ma[ma:end, :] 
 
   moving!(df_new, ma_col, std, 20) 
-  df_new["up_bband"] = df_new[ma_col] + df_new["std_20"] * width
-  df_new["dn_bband"] = df_new[ma_col] - df_new["std_20"] * width
+  df_new["up_bband"] = df_new[ma_col] + df_new[sd_col] * width
+  df_new["dn_bband"] = df_new[ma_col] - df_new[sd_col] * width
   df_new
 end
 
