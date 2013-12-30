c6ad3f6adeb20dd730b3167d5bb19438735eda3f
milktrader <milktrader@gmail.com>
2013-12-30 15:41:59 -0500

    added wilder smoothing to ema methods, need tests

--- src/movingaverages.jl
+++ src/movingaverages.jl
@@ -19,45 +19,41 @@ end
 function ema{T,V}(sa::Array{SeriesPair{T,V},1}, n::Int; method="simple")
   if method == "simple"
     k   = 2/(n+1)
-    m   = sma(sa, n) 
-    res = SeriesPair{T,V}[]
-    push!(res, SeriesPair(index(sa)[n], m[1].value)) # first one is always simple 
-
-    for i = n+1:length(sa)
-      sp = SeriesPair(sa[i].index, (sa[i].value * k + res[i-n].value * (1-k)))
-      push!(res, sp)
-    end
-    res
-
   elseif method == "wilder"
-    println("")
-    print_with_color(:blue, "support for  wilder method is planned.")
-    println("")
-
+    k   = 1/n
   else
     error("method is not supported.")
   end
+
+  m   = sma(sa, n) 
+  res = SeriesPair{T,V}[]
+  push!(res, SeriesPair(index(sa)[n], m[1].value)) # first one is always simple 
+ 
+  for i = n+1:length(sa)
+    sp = SeriesPair(sa[i].index, (sa[i].value * k + res[i-n].value * (1-k)))
+    push!(res, sp)
+  end
+  res
 end
 
 function ema{T}(a::Array{T,1}, n::Int; method="simple")
   if method == "simple"
     k   = 2/(n+1)
-    m   = sma(a, n) 
-    res = Float64[]
-    push!(res, m[1]) # first one is always simple 
-
-    for i = n+1:size(a, 1)
-      val = a[i] * k + res[i-n] * (1-k)
-      push!(res, val)
-    end
-    res
-
   elseif method == "wilder"
-    println("")
-    print_with_color(:blue, "support for  wilder method is planned.")
-    println("")
-
+    k   = 1/n
   else
     error("method is not supported.")
   end
+ 
+  m   = sma(a, n) 
+  res = Float64[]
+  push!(res, m[1]) # first one is always simple 
+
+  for i = n+1:size(a, 1)
+    val = a[i] * k + res[i-n] * (1-k)
+    push!(res, val)
+  end
+  res
+
+
 end
