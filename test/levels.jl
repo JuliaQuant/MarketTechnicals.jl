2b835ba0f467a14fa2d84ba396aa8017b12f622b
milktrader <milktrader@gmail.com>
2014-02-21 22:24:15 -0500

    woodiespivots tests stubbed

--- test/levels.jl
+++ test/levels.jl
@@ -16,13 +16,13 @@ facts("Levels") do
   context("woodiespivots") do
 #    #  @fact_approx_eq  97.37500000000001 value(wr4)[2]   # values NEED to be verified with online calculators 
 #    #  @fact_approx_eq   88.62500000000001 value(ws4)[2]  
-#    @fact value(wr3)[2]  => roughly(109.450)  
-#    @fact value(wr2)[2]  => roughly(107.765)  
-#    @fact value(wr1)[2]  => roughly(106.630)  
-#    @fact value(wp)[2]   => roughly(104.945)    
-#    @fact value(ws1)[2]  => roughly(103.810)  
-#    @fact value(ws2)[2]  => roughly(102.125)  
-#    @fact value(ws3)[2]  => roughly(100.990)  
-#    @fact index(wp)[end] => lastday
+    @fact woodiespivots(ohlc)["r3"].values[1]    => roughly(109.450)  
+    @fact woodiespivots(ohlc)["r2"].values[1]    => roughly(107.765)  
+    @fact woodiespivots(ohlc)["r1"].values[1]    => roughly(106.630)  
+    @fact woodiespivots(ohlc)["pivot"].values[1] => roughly(104.945)    
+    @fact woodiespivots(ohlc)["s1"].values[1]    => roughly(103.810)  
+    @fact woodiespivots(ohlc)["s2"].values[1]    => roughly(102.125)  
+    @fact woodiespivots(ohlc)["s3"].values[1]    => roughly(100.990)  
+    @fact woodiespivots(ohlc).timestamp[end]       => lastday
   end
 end
