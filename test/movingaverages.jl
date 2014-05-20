fe8a682f63b0f580584474aa0d02bebf5e70189f
milktrader <milktrader@gmail.com>
2014-05-20 16:15:44 -0400

    majority of tests converted

--- test/movingaverages.jl
+++ test/movingaverages.jl
@@ -3,47 +3,47 @@ using MarketData
 facts("Moving Averages") do
 
   context("sma") do  
-    @fact sma(cl, 10).values[1]      => roughly(108.893)  # TTR value 108.893
-    @fact sma(cl, 10).values[2]      => roughly(109.441)  # TTR value 109.441
-    @fact sma(cl, 10).values[3]      => roughly(109.896)  # TTR value 109.896
-    @fact sma(cl, 10).values[495]    => roughly(122.685)  # TTR value 122.685
-    @fact sma(cl, 10).values[496]    => roughly(122.698)  # TTR value 122.698
-    @fact sma(cl, 10).timestamp[496] => lastday
+    @fact sma(cl, 10).values[1]      => roughly(98.782)  # TTR value 98.782
+    @fact sma(cl, 10).values[2]      => roughly(97.982)  # TTR value 97.982
+    @fact sma(cl, 10).values[3]      => roughly(98.388)  # TTR value 98.388
+    @fact sma(cl, 10).values[490]    => roughly(21.266)  # TTR value 21.266
+    @fact sma(cl, 10).values[491]    => roughly(21.417)  # TTR value 21.417
+    @fact sma(cl, 10).timestamp[491] => date(2001,12,31)
   end 
   
   context("ema") do 
-    @fact ema(cl, 10).values[1]      => roughly(108.893)   # TTR value 108.8930
-    @fact ema(cl, 10).values[2]      => roughly(109.2215)  # TTR value 109.2215
-    @fact ema(cl, 10).values[3]      => roughly(109.5576)  # TTR value 109.5576
-    @fact ema(cl, 10).values[495]    => roughly(122.7024)  # TTR value 122.7024
-    @fact ema(cl, 10).values[496]    => roughly(122.6747)  # TTR value 122.6747
-    @fact ema(cl, 10).timestamp[496] => lastday
+    @fact ema(cl, 10).values[1]      => roughly(98.78200)   # TTR value 98.78200
+    @fact ema(cl, 10).values[2]      => roughly(99.71982)   # TTR value 99.71982
+    @fact ema(cl, 10).values[3]      => roughly(100.96349)  # TTR value 100.96349
+    @fact ema(cl, 10).values[490]    => roughly(21.58580)   # TTR value 21.58580
+    @fact ema(cl, 10).values[491]    => roughly(21.64293)   # TTR value 21.64293
+    @fact ema(cl, 10).timestamp[491] => date(2001,12,31)
   end
 
   context("ema wilder true") do 
-    @fact ema(cl, 10, wilder=true).values[1]      => roughly(108.893)   # TTR value 108.8930
-    @fact ema(cl, 10, wilder=true).values[2]      => roughly(109.0737)  # TTR value 109.0737
-    @fact ema(cl, 10, wilder=true).values[3]      => roughly(109.2733)  # TTR value 109.2733
-    @fact ema(cl, 10, wilder=true).values[495]    => roughly(123.0738)  # TTR value 123.0738
-    @fact ema(cl, 10, wilder=true).values[496]    => roughly(123.0214)  # TTR value 123.0214
-    @fact ema(cl, 10, wilder=true).timestamp[496] => lastday
+    @fact ema(cl, 10, wilder=true).values[1]      => roughly(98.7820)  # TTR value 98.7820
+    @fact ema(cl, 10, wilder=true).values[2]      => roughly(99.2978)  # TTR value 99.2978
+    @fact ema(cl, 10, wilder=true).values[3]      => roughly(100.0240)  # TTR value 100.0240
+    @fact ema(cl, 10, wilder=true).values[490]    => roughly(21.34556)  # TTR value 21.34556
+    @fact ema(cl, 10, wilder=true).values[491]    => roughly(21.40100)  # TTR value 21.40100
+    @fact ema(cl, 10, wilder=true).timestamp[491] => date(2001,12,31)
   end
 
   context("Array dispatch is correct too") do 
-    @fact sma(cl.values, 10)[1]                => roughly(108.893)   # same values as above TimeArray dispatch
-    @fact sma(cl.values, 10)[2]                => roughly(109.441)   
-    @fact sma(cl.values, 10)[3]                => roughly(109.896)   
-    @fact sma(cl.values, 10)[495]              => roughly(122.685)   
-    @fact sma(cl.values, 10)[496]              => roughly(122.698)   
-    @fact ema(cl.values, 10)[1]                => roughly(108.893)   
-    @fact ema(cl.values, 10)[2]                => roughly(109.2215)  
-    @fact ema(cl.values, 10)[3]                => roughly(109.5576)  
-    @fact ema(cl.values, 10)[495]              => roughly(122.7024)  
-    @fact ema(cl.values, 10)[496]              => roughly(122.6747)  
-    @fact ema(cl.values, 10, wilder=true)[1]   => roughly(108.893)   
-    @fact ema(cl.values, 10, wilder=true)[2]   => roughly(109.0737)  
-    @fact ema(cl.values, 10, wilder=true)[3]   => roughly(109.2733)  
-    @fact ema(cl.values, 10, wilder=true)[495] => roughly(123.0738)  
-    @fact ema(cl.values, 10, wilder=true)[496] => roughly(123.0214)  
+    @fact sma(cl.values, 10)[1]                => roughly(98.782)   # same values as above TimeArray dispatch
+    @fact sma(cl.values, 10)[2]                => roughly(97.982)   
+    @fact sma(cl.values, 10)[3]                => roughly(98.388)   
+    @fact sma(cl.values, 10)[490]              => roughly(21.266)   
+    @fact sma(cl.values, 10)[491]              => roughly(21.417)   
+    @fact ema(cl.values, 10)[1]                => roughly(98.78200)   
+    @fact ema(cl.values, 10)[2]                => roughly(99.71982)  
+    @fact ema(cl.values, 10)[3]                => roughly(100.96349) 
+    @fact ema(cl.values, 10)[490]              => roughly(21.58580)  
+    @fact ema(cl.values, 10)[491]              => roughly(21.64293)  
+    @fact ema(cl.values, 10, wilder=true)[1]   => roughly(98.7820)
+    @fact ema(cl.values, 10, wilder=true)[2]   => roughly(99.2978)  
+    @fact ema(cl.values, 10, wilder=true)[3]   => roughly(100.0240)  
+    @fact ema(cl.values, 10, wilder=true)[490] => roughly(21.34556)  
+    @fact ema(cl.values, 10, wilder=true)[491] => roughly(21.40100)  
   end
 end
