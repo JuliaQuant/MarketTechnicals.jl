e4a1d3bfaa85c4a736729a14e4901581ef2d73d0
milktrader <milktrader@gmail.com>
2015-09-15 09:14:23 -0400

    support for Julia 0.4 only

--- test/volume.jl
+++ test/volume.jl
@@ -3,14 +3,14 @@ using MarketData
 facts("Volume") do
 
     context("obv") do
-        @fact obv(ohlcv).values[1]     => 4783900 # TTR value is 4783900
-        @fact obv(ohlcv).values[12]    => 9390200 # TTR value is 2000-01-19   9390200
-        @fact obv(ohlcv).timestamp[12] => Date(2000,1,19)
+        @fact obv(ohlcv).values[1]     --> 4783900 # TTR value is 4783900
+        @fact obv(ohlcv).values[12]    --> 9390200 # TTR value is 2000-01-19   9390200
+        @fact obv(ohlcv).timestamp[12] --> Date(2000,1,19)
     end 
 
     context("vwap") do
-       @fact vwap(ohlcv).values[1]    => roughly(97.9215, atol=.01)  # TTR value 2000-01-14 97.92154
-       @fact vwap(ohlcv).values[end]  => roughly(21.4446, atol=.01)  # TTR value  21.44458
-       @fact vwap(ohlcv).timestamp[1] => Date(2000,1,14)
+       @fact vwap(ohlcv).values[1]    --> roughly(97.9215, atol=.01)  # TTR value 2000-01-14 97.92154
+       @fact vwap(ohlcv).values[end]  --> roughly(21.4446, atol=.01)  # TTR value  21.44458
+       @fact vwap(ohlcv).timestamp[1] --> Date(2000,1,14)
     end
 end
