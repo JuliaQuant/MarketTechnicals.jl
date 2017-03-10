4812e185467feb127e5df7210c2dc45681c78594
Iblis Lin <iblis@hs.ntnu.edu.tw>
2017-03-10 21:21:08 +0800

    docs: migrate getting_started

--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,12 @@
 using Documenter
 using MarketTechnicals
 
+
 makedocs(
     format = :html,
     sitename = "MarketTechnicals.jl",
     pages = [
         "index.md",
+        "getting_started.md",
     ]
 )
