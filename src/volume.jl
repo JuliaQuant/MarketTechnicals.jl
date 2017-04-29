990d0d4c1d0e56c83d04eb9b7281501a30095d6e
Iblis Lin <iblis@hs.ntnu.edu.tw>
2017-04-29 17:04:48 +0800

    style: cleanup trailing space for volume.jl

--- src/volume.jl
+++ src/volume.jl
@@ -3,8 +3,8 @@ function obv{T,N}(ohlcv::TimeArray{T,N}; price="Close", v="Volume")
     ret    = percentchange(ohlcv[price])
     vol    = zeros(length(ohlcv))
     vol[1] = ohlcv[v].values[1]
-    
-    for i=2:length(ohlcv) 
+
+    for i=2:length(ohlcv)
       if ret.values[i-1] >= 0
         vol[i] += ohlcv[v].values[i]
       else
@@ -14,20 +14,20 @@ function obv{T,N}(ohlcv::TimeArray{T,N}; price="Close", v="Volume")
 
     TimeArray(ohlcv.timestamp, cumsum(vol), ["obv"], ohlcv.meta)
 end
- 
+
 function vwap{T,N}(ohlcv::TimeArray{T,N}, n::Int; price="Close", v="Volume")
-  
+
     p   = ohlcv[price]
     q   = ohlcv[v]
     ∑PQ = moving(p.*q, sum, n)
     ∑Q  = moving(q, sum, n)
     val = ∑PQ ./ ∑Q
- 
+
     TimeArray(val.timestamp, val.values, ["vwap"], ohlcv.meta)
 end
- 
+
 vwap{T,N}(ohlcv::TimeArray{T,N}) = vwap(ohlcv, 10)
- 
+
 function advance_decline(x)
     #code here
 end
