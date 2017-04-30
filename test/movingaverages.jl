ef727b84e00e60981392417370a770a44f7b3303
Iblis Lin <iblis@hs.ntnu.edu.tw>
2017-04-30 20:40:21 +0800

    test: migrate movingaverages.jl, apply 0.6 syntax

--- test/movingaverages.jl
+++ test/movingaverages.jl
@@ -1,92 +1,110 @@
 using MarketData
 
-facts("Moving averages on TimeArrays") do
 
-    context("sma") do
-        @fact sma(cl, 10).values[1]        --> roughly(98.782, atol=.01)  # TTR value 98.782
-        @fact sma(cl, 10).values[2]        --> roughly(97.982, atol=.01)  # TTR value 97.982
-        @fact sma(cl, 10).values[3]        --> roughly(98.388, atol=.01)  # TTR value 98.388
-        @fact sma(cl, 10).values[490]      --> roughly(21.266, atol=.01)  # TTR value 21.266
-        @fact sma(cl, 10).values[491]      --> roughly(21.417, atol=.01)  # TTR value 21.417
-        @fact sma(cl, 10).timestamp[491]   --> Date(2001,12,31)
-        @fact sma(ohlc, 10).values[1,:]    --> roughly([100.692, 103.981, 96.001, 98.782] , atol=.01)  # TTR value 98.782
-        @fact sma(ohlc, 10).values[2,:]    --> roughly([100.304, 103.331, 95.876, 97.982], atol=.01)  # TTR value 97.982
-        @fact sma(ohlc, 10).values[3,:]    --> roughly([100.041, 103.144, 96.095,98.388], atol=.01)  # TTR value 98.388
-        @fact sma(ohlc, 10).values[490,:]  --> roughly([21.081, 21.685, 20.797, 21.266], atol=.01)  # TTR value 21.266
-        @fact sma(ohlc, 10).values[491,:]  --> roughly([21.259, 21.868, 20.971, 21.417], atol=.01)  # TTR value 21.417
-        @fact sma(ohlc, 10).timestamp[491] --> Date(2001,12,31)
+@testset "Moving averages on TimeArrays"  begin
+
+    @testset "sma"  begin
+        ta = sma(cl, 10)
+        @test ta.values[1]       ≈ 98.782 atol=.01  # TTR value 98.782
+        @test ta.values[2]       ≈ 97.982 atol=.01  # TTR value 97.982
+        @test ta.values[3]       ≈ 98.388 atol=.01  # TTR value 98.388
+        @test ta.values[490]     ≈ 21.266 atol=.01  # TTR value 21.266
+        @test ta.values[491]     ≈ 21.417 atol=.01  # TTR value 21.417
+        @test ta.timestamp[491] == Date(2001,12,31)
+
+        ta = sma(ohlc, 10)
+        @test ta.values[1,:]     ≈ [100.692, 103.981, 96.001, 98.782] atol=.01
+        @test ta.values[2,:]     ≈ [100.304, 103.331, 95.876, 97.982] atol=.01
+        @test ta.values[3,:]     ≈ [100.041, 103.144, 96.095,98.388]  atol=.01
+        @test ta.values[490,:]   ≈ [21.081, 21.685, 20.797, 21.266]   atol=.01
+        @test ta.values[491,:]   ≈ [21.259, 21.868, 20.971, 21.417]   atol=.01
+        @test ta.timestamp[491] == Date(2001,12,31)
     end
 
-    context("ema") do
-        @fact ema(cl, 10).values[1]        --> roughly(98.782, atol=.01)   # TTR value 98.78200
-        @fact ema(cl, 10).values[2]        --> roughly(99.719, atol=.01)   # TTR value 99.71982
-        @fact ema(cl, 10).values[3]        --> roughly(100.963, atol=.01)  # TTR value 100.96349
-        @fact ema(cl, 10).values[490]      --> roughly(21.585, atol=.01)   # TTR value 21.58580
-        @fact ema(cl, 10).values[491]      --> roughly(21.642, atol=.01)   # TTR value 21.64293
-        @fact ema(cl, 10).timestamp[491]   --> Date(2001,12,31)
-        @fact ema(ohlc, 10).values[1,:]    --> roughly([100.692, 103.981, 96.001, 98.782], atol=.01)   # TTR value 98.78200
-        @fact ema(ohlc, 10).values[2,:]    --> roughly([100.748, 104.348, 96.808, 99.719], atol=.01)   # TTR value 99.71982
-        @fact ema(ohlc, 10).values[3,:]    --> roughly([101.634, 105.148, 98.003, 100.963], atol=.01)  # TTR value 100.96349
-        @fact ema(ohlc, 10).values[490,:]  --> roughly([21.369, 22.030, 21.125, 21.585], atol=.01)   # TTR value 21.58580
-        @fact ema(ohlc, 10).values[491,:]  --> roughly([21.576, 22.145, 21.253, 21.642], atol=.01)   # TTR value 21.64293
-        @fact ema(ohlc, 10).timestamp[491] --> Date(2001,12,31)
+    @testset "ema"  begin
+        ta = ema(cl, 10)
+        @test ta.values[1]       ≈ 98.782  atol=.01  # TTR value 98.78200
+        @test ta.values[2]       ≈ 99.719  atol=.01  # TTR value 99.71982
+        @test ta.values[3]       ≈ 100.963 atol=.01  # TTR value 100.96349
+        @test ta.values[490]     ≈ 21.585  atol=.01  # TTR value 21.58580
+        @test ta.values[491]     ≈ 21.642  atol=.01  # TTR value 21.64293
+        @test ta.timestamp[491] == Date(2001,12,31)
+
+        ta = ema(ohlc, 10)
+        @test ta.values[1,:]     ≈ [100.692, 103.981, 96.001, 98.782]  atol=.01
+        @test ta.values[2,:]     ≈ [100.748, 104.348, 96.808, 99.719]  atol=.01
+        @test ta.values[3,:]     ≈ [101.634, 105.148, 98.003, 100.963] atol=.01
+        @test ta.values[490,:]   ≈ [21.369, 22.030, 21.125, 21.585]    atol=.01
+        @test ta.values[491,:]   ≈ [21.576, 22.145, 21.253, 21.642]    atol=.01
+        @test ta.timestamp[491] == Date(2001,12,31)
     end
 
-    context("ema wilder true") do
-        @fact ema(cl, 10, wilder=true).values[1]        --> roughly(98.782, atol=.01)  # TTR value 98.7820
-        @fact ema(cl, 10, wilder=true).values[2]        --> roughly(99.297, atol=.01)  # TTR value 99.2978
-        @fact ema(cl, 10, wilder=true).values[3]        --> roughly(100.024, atol=.01)  # TTR value 100.0240
-        @fact ema(cl, 10, wilder=true).values[490]      --> roughly(21.345, atol=.01)  # TTR value 21.34556
-        @fact ema(cl, 10, wilder=true).values[491]      --> roughly(21.401, atol=.01)  # TTR value 21.40100
-        @fact ema(cl, 10, wilder=true).timestamp[491]   --> Date(2001,12,31)
-        @fact ema(ohlc, 10, wilder=true).values[1,:]    --> roughly([100.692, 103.981, 96.001, 98.782], atol=.01)  # TTR value 98.7820
-        @fact ema(ohlc, 10, wilder=true).values[2,:]    --> roughly([100.723, 104.183, 96.444, 99.297], atol=.01)  # TTR value 99.2978
-        @fact ema(ohlc, 10, wilder=true).values[3,:]    --> roughly([101.213, 104.64, 97.138, 100.024], atol=.01)  # TTR value 100.0240
-        @fact ema(ohlc, 10, wilder=true).values[490,:]  --> roughly([21.184, 21.776, 20.847, 21.345], atol=.01)  # TTR value 21.34556
-        @fact ema(ohlc, 10, wilder=true).values[491,:]  --> roughly([21.317, 21.865, 20.945, 21.401], atol=.01)  # TTR value 21.40100
-        @fact ema(ohlc, 10, wilder=true).timestamp[491] --> Date(2001,12,31)
+    @testset "ema wilder true"  begin
+        ta = ema(cl, 10, wilder=true)
+        @test ta.values[1]       ≈ 98.782  atol=.01  # TTR value 98.7820
+        @test ta.values[2]       ≈ 99.297  atol=.01  # TTR value 99.2978
+        @test ta.values[3]       ≈ 100.024 atol=.01  # TTR value 100.0240
+        @test ta.values[490]     ≈ 21.345  atol=.01  # TTR value 21.34556
+        @test ta.values[491]     ≈ 21.401  atol=.01  # TTR value 21.40100
+        @test ta.timestamp[491] == Date(2001,12,31)
+
+        ta = ema(ohlc, 10, wilder=true)
+        @test ta.values[1,:]     ≈ [100.692, 103.981, 96.001, 98.782] atol=.01
+        @test ta.values[2,:]     ≈ [100.723, 104.183, 96.444, 99.297] atol=.01
+        @test ta.values[3,:]     ≈ [101.213, 104.64, 97.138, 100.024] atol=.01
+        @test ta.values[490,:]   ≈ [21.184, 21.776, 20.847, 21.345]   atol=.01
+        @test ta.values[491,:]   ≈ [21.317, 21.865, 20.945, 21.401]   atol=.01
+        @test ta.timestamp[491] == Date(2001,12,31)
     end
 end
 
-facts("Moving averages on arrays") do
+@testset "Moving averages on arrays"  begin
 
-    context("Array dispatch on sma") do
-        @fact sma(cl.values, 10)[1]       --> roughly(98.782, atol=.01)   # same values as above TimeArray dispatch
-        @fact sma(cl.values, 10)[2]       --> roughly(97.982, atol=.01)
-        @fact sma(cl.values, 10)[3]       --> roughly(98.388, atol=.01)
-        @fact sma(cl.values, 10)[490]     --> roughly(21.266, atol=.01)
-        @fact sma(cl.values, 10)[491]     --> roughly(21.417, atol=.01)
-        @fact sma(ohlc.values, 10)[1,:]   --> roughly([100.692, 103.981, 96.001, 98.782] , atol=.01)  # TTR value 98.782
-        @fact sma(ohlc.values, 10)[2,:]   --> roughly([100.304, 103.331, 95.876, 97.982], atol=.01)  # TTR value 97.982
-        @fact sma(ohlc.values, 10)[3,:]   --> roughly([100.041, 103.144, 96.095,98.388], atol=.01)  # TTR value 98.388
-        @fact sma(ohlc.values, 10)[490,:] --> roughly([21.081, 21.685, 20.797, 21.266], atol=.01)  # TTR value 21.266
-        @fact sma(ohlc.values, 10)[491,:] --> roughly([21.259, 21.868, 20.971, 21.417], atol=.01)  # TTR value 21.417
+    @testset "Array dispatch on sma"  begin
+        arr = sma(cl.values, 10)
+        @test arr[1]   ≈ 98.782 atol=.01  # same values as above TimeArray dispatch
+        @test arr[2]   ≈ 97.982 atol=.01
+        @test arr[3]   ≈ 98.388 atol=.01
+        @test arr[490] ≈ 21.266 atol=.01
+        @test arr[491] ≈ 21.417 atol=.01
+
+        arr = sma(ohlc.values, 10)
+        @test arr[1,:]   ≈ [100.692, 103.981, 96.001, 98.782] atol=.01
+        @test arr[2,:]   ≈ [100.304, 103.331, 95.876, 97.982] atol=.01
+        @test arr[3,:]   ≈ [100.041, 103.144, 96.095,98.388]  atol=.01
+        @test arr[490,:] ≈ [21.081, 21.685, 20.797, 21.266]   atol=.01
+        @test arr[491,:] ≈ [21.259, 21.868, 20.971, 21.417]   atol=.01
     end
 
-    context("Array dispatch on ema") do
-        @fact ema(cl.values, 10)[1]        --> roughly(98.782, atol=.01)
-        @fact ema(cl.values, 10)[2]        --> roughly(99.719, atol=.01)
-        @fact ema(cl.values, 10)[3]        --> roughly(100.963, atol=.01)
-        @fact ema(cl.values, 10)[490]      --> roughly(21.585, atol=.01)
-        @fact ema(cl.values, 10)[491]      --> roughly(21.642, atol=.01)
-        @fact ema(ohlc, 10).values[1,:]    --> roughly([100.692, 103.981, 96.001, 98.782], atol=.01)   # TTR value 98.78200
-        @fact ema(ohlc, 10).values[2,:]    --> roughly([100.748, 104.348, 96.808, 99.719], atol=.01)   # TTR value 99.71982
-        @fact ema(ohlc, 10).values[3,:]    --> roughly([101.634, 105.148, 98.003, 100.963], atol=.01)  # TTR value 100.96349
-        @fact ema(ohlc, 10).values[490,:]  --> roughly([21.369, 22.030, 21.125, 21.585], atol=.01)   # TTR value 21.58580
-        @fact ema(ohlc, 10).values[491,:]  --> roughly([21.576, 22.145, 21.253, 21.642], atol=.01)   # TTR value 21.64293
-        @fact ema(ohlc, 10).timestamp[491] --> Date(2001,12,31)
+    @testset "Array dispatch on ema"  begin
+        arr = ema(cl.values, 10)
+        @test arr[1]   ≈ 98.782  atol=.01
+        @test arr[2]   ≈ 99.719  atol=.01
+        @test arr[3]   ≈ 100.963 atol=.01
+        @test arr[490] ≈ 21.585  atol=.01
+        @test arr[491] ≈ 21.642  atol=.01
+
+        arr = ema(ohlc.values, 10)
+        @test arr[1,:]   ≈ [100.692, 103.981, 96.001, 98.782]  atol=.01
+        @test arr[2,:]   ≈ [100.748, 104.348, 96.808, 99.719]  atol=.01
+        @test arr[3,:]   ≈ [101.634, 105.148, 98.003, 100.963] atol=.01
+        @test arr[490,:] ≈ [21.369, 22.030, 21.125, 21.585]    atol=.01
+        @test arr[491,:] ≈ [21.576, 22.145, 21.253, 21.642]    atol=.01
     end
 
-    context("Array dispatch on ema wilder true") do
-        @fact ema(cl.values, 10, wilder=true)[1]       --> roughly(98.782, atol=.01)
-        @fact ema(cl.values, 10, wilder=true)[2]       --> roughly(99.297, atol=.01)
-        @fact ema(cl.values, 10, wilder=true)[3]       --> roughly(100.024, atol=.01)
-        @fact ema(cl.values, 10, wilder=true)[490]     --> roughly(21.345, atol=.01)
-        @fact ema(cl.values, 10, wilder=true)[491]     --> roughly(21.401, atol=.01)
-        @fact ema(ohlc.values, 10, wilder=true)[1,:]   --> roughly([100.692, 103.981, 96.001, 98.782], atol=.01)  # TTR value 98.7820
-        @fact ema(ohlc.values, 10, wilder=true)[2,:]   --> roughly([100.723, 104.183, 96.444, 99.297], atol=.01)  # TTR value 99.2978
-        @fact ema(ohlc.values, 10, wilder=true)[3,:]   --> roughly([101.213, 104.64, 97.138, 100.024], atol=.01)  # TTR value 100.0240
-        @fact ema(ohlc.values, 10, wilder=true)[490,:] --> roughly([21.184, 21.776, 20.847, 21.345], atol=.01)  # TTR value 21.34556
-        @fact ema(ohlc.values, 10, wilder=true)[491,:] --> roughly([21.317, 21.865, 20.945, 21.401], atol=.01)  # TTR value 21.40100
+    @testset "Array dispatch on ema wilder true"  begin
+        arr = ema(cl.values, 10, wilder=true)
+        @test arr[1]   ≈ 98.782  atol=.01
+        @test arr[2]   ≈ 99.297  atol=.01
+        @test arr[3]   ≈ 100.024 atol=.01
+        @test arr[490] ≈ 21.345  atol=.01
+        @test arr[491] ≈ 21.401  atol=.01
+
+        arr = ema(ohlc.values, 10, wilder=true)
+        @test arr[1,:]   ≈ [100.692, 103.981, 96.001, 98.782] atol=.01
+        @test arr[2,:]   ≈ [100.723, 104.183, 96.444, 99.297] atol=.01
+        @test arr[3,:]   ≈ [101.213, 104.64, 97.138, 100.024] atol=.01
+        @test arr[490,:] ≈ [21.184, 21.776, 20.847, 21.345]   atol=.01
+        @test arr[491,:] ≈ [21.317, 21.865, 20.945, 21.401]   atol=.01
     end
 end
