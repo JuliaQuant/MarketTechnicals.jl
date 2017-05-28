802a8099b810c0d49c482a1dd094b740ff1c40da
Iblis Lin <iblis@hs.ntnu.edu.tw>
2017-05-29 01:51:15 +0800

    utils: new padding macro

--- src/MarketTechnicals.jl
+++ src/MarketTechnicals.jl
@@ -14,11 +14,12 @@ export sma, ema, kama,
        floorpivots, woodiespivots,
        typical
 
+include("utilities.jl")  # we need macro in compiling stage
+
 include("candlesticks.jl")
 include("levels.jl")
 include("movingaverages.jl")
 include("momentum.jl")
-include("utilities.jl")
 include("volatility.jl")
 include("volume.jl")
 
--- src/utilities.jl
+++ src/utilities.jl
@@ -42,3 +42,67 @@ julia> gen_colnames(["Open", "Close"], ["macd", "dif", "sig"])
 function gen_colnames(orig::Vector{String}, suffix::Vector{String})
    vec(["$o\_$s" for o ∈ orig, s ∈ suffix])
 end
+
+gen_colnames(orig::Vector{String}, suffix::String) = gen_colnames(orig, [suffix])
+
+function add_padding_call(expr::Expr)
+    if expr.head != :call
+        return [add_padding_call(arg) for arg ∈ expr.args if isa(arg, Expr)]
+    end
+
+    func = expr.args[1]
+    if func ∈ (:diff, :lag, :lead, :moving, :percentchange)
+        push!(expr.args, Expr(:kw, :padding, :padding))
+    end
+
+    return [add_padding_call(arg)
+            for arg ∈ expr.args[2:end-1] if isa(arg, Expr)]
+end
+
+"""
+This macro will append a keyword argument `padding=true` to
+function parameters, and append `padding` to all related call
+in function body. For the listed related call, please check out
+`add_padding_call`.
+
+e.g.
+
+```
+@padding function f(ta, n)
+    ...
+    moving(...)
+    ...
+    lag(...)
+    ...
+end
+```
+
+will make `f` compiled as following
+
+```
+function sma(ta, n; padding=false)
+    ...
+    moving(..., padding=padding)
+    ....
+    lag(..., padding=padding)
+    ...
+end
+```
+
+"""
+macro padding(f)
+    @assert f.head == :function
+
+    # add :padding keyword argument in function signature
+    params_ast = f.args[1]
+    kw_node = Expr(:kw, :padding, false)
+
+    if (length(params_ast.args) < 2) || (params_ast.args[2].head != :parameters)
+        insert!(params_ast.args, 2, Expr(:parameters, kw_node))
+    else
+        push!(params_ast.args[2], kw_node)
+    end
+
+    add_padding_call(f.args[2])
+    return f
+end
