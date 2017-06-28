cafbe604387a80acadb0110c8892ec1c2dcfcc10
Iblis Lin <iblis@hs.ntnu.edu.tw>
2017-06-28 23:58:49 +0800

    test: migrate levels.jl (#70)

--- test/levels.jl
+++ test/levels.jl
@@ -1,26 +1,39 @@
-facts("Levels") do
+using Base.Test
 
-    context("floor pivots") do
-        @fact floorpivots(ohlc)["r3"].values[1]    --> roughly(123.310, atol=.01)              # values verified by various website calculators
-        @fact floorpivots(ohlc)["r2"].values[1]    --> roughly(119.52, atol=.01)
-        @fact floorpivots(ohlc)["r1"].values[1]    --> roughly(115.73, atol=.01)
-        @fact floorpivots(ohlc)["pivot"].values[1] --> roughly(108.71, atol=.01)
-        @fact floorpivots(ohlc)["s1"].values[1]    --> roughly(104.92, atol=.01)
-        @fact floorpivots(ohlc)["s2"].values[1]    --> roughly(97.900, atol=.01)
-        @fact floorpivots(ohlc)["s3"].values[1]    --> roughly(94.110, atol=.01)
-        @fact floorpivots(ohlc).timestamp[end]     --> Date(2001,12,31)
-    end
+using MarketData
+using TimeSeries
 
-    context("woodiespivots") do
-  #    #  @fact_approx_eq  97.37500000000001 value(wr4)[2]   # values NEED to be verified with online calculators
-  #    #  @fact_approx_eq   88.62500000000001 value(ws4)[2]
-        @fact woodiespivots(ohlc)["r3"].values[1]    --> roughly(124.465, atol=.01)
-        @fact woodiespivots(ohlc)["r2"].values[1]    --> roughly(118.480, atol=.01)
-        @fact woodiespivots(ohlc)["r1"].values[1]    --> roughly(113.655, atol=.01)
-        @fact woodiespivots(ohlc)["pivot"].values[1] --> roughly(107.670, atol=.01)
-        @fact woodiespivots(ohlc)["s1"].values[1]    --> roughly(102.845, atol=.01)
-        @fact woodiespivots(ohlc)["s2"].values[1]    --> roughly(96.8625, atol=.01)
-        @fact woodiespivots(ohlc)["s3"].values[1]    --> roughly(92.035, atol=.01)
-        @fact woodiespivots(ohlc).timestamp[end]     --> Date(2001,12,31)
-    end
+using MarketTechnicals
+
+
+@testset "Levels" begin
+
+
+@testset "floor pivots" begin
+    # values verified by various website calculators
+    @test isapprox(floorpivots(ohlc)["r3"].values[1]   , 123.310, atol=.01)
+    @test isapprox(floorpivots(ohlc)["r2"].values[1]   , 119.52, atol=.01)
+    @test isapprox(floorpivots(ohlc)["r1"].values[1]   , 115.73, atol=.01)
+    @test isapprox(floorpivots(ohlc)["pivot"].values[1], 108.71, atol=.01)
+    @test isapprox(floorpivots(ohlc)["s1"].values[1]   , 104.92, atol=.01)
+    @test isapprox(floorpivots(ohlc)["s2"].values[1]   , 97.900, atol=.01)
+    @test isapprox(floorpivots(ohlc)["s3"].values[1]   , 94.110, atol=.01)
+    @test floorpivots(ohlc).timestamp[end] == Date(2001,12,31)
 end
+
+
+@testset "woodiespivots" begin
+    # @test_approx_eq  97.37500000000001 value(wr4)[2]   # values NEED to be verified with online calculators
+    # @test_approx_eq   88.62500000000001 value(ws4)[2]
+    @test isapprox(woodiespivots(ohlc)["r3"].values[1]   , 124.465, atol=.01)
+    @test isapprox(woodiespivots(ohlc)["r2"].values[1]   , 118.480, atol=.01)
+    @test isapprox(woodiespivots(ohlc)["r1"].values[1]   , 113.655, atol=.01)
+    @test isapprox(woodiespivots(ohlc)["pivot"].values[1], 107.670, atol=.01)
+    @test isapprox(woodiespivots(ohlc)["s1"].values[1]   , 102.845, atol=.01)
+    @test isapprox(woodiespivots(ohlc)["s2"].values[1]   , 96.8625, atol=.01)
+    @test isapprox(woodiespivots(ohlc)["s3"].values[1]   , 92.035, atol=.01)
+    @test woodiespivots(ohlc).timestamp[end] == Date(2001,12,31)
+end
+
+
+end  # @testset "Levels"
