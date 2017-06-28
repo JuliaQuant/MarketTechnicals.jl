f9b4eed64fc6eec2fd745d2b079b934b271ef788
Iblis Lin <iblis@hs.ntnu.edu.tw>
2017-06-28 23:48:53 +0800

    test: migrate candlesticks.jl (#70)

--- test/candlesticks.jl
+++ test/candlesticks.jl
@@ -1,10 +1,19 @@
-using MarketData, TimeSeries
+using Base.Test
 
-facts("Candlesticks") do
+using MarketData
+using TimeSeries
 
-    context("doji") do
-        @fact ohlc[find(doji(ohlc))].timestamp[1] --> Date(2000, 5, 30)
-        @fact ohlc[find(doji(ohlc))].timestamp[2] --> Date(2000, 12, 11)
-        @fact length(ohlc[find(doji(ohlc))])      --> 5
-    end
+using MarketTechnicals
+
+
+@testset "Candlesticks" begin
+
+
+@testset "doji" begin
+    @test ohlc[find(doji(ohlc))].timestamp[1] == Date(2000, 5, 30)
+    @test ohlc[find(doji(ohlc))].timestamp[2] == Date(2000, 12, 11)
+    @test length(ohlc[find(doji(ohlc))])      == 5
 end
+
+
+end  # @testset "Candlesticks"
