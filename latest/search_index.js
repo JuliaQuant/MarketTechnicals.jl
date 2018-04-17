dd54ed6b30f4d91b6ee4b9a161aba5f24ede6740
autodocs <autodocs>
2018-04-17 08:22:43 +0000

    build based on da9320d

--- latest/search_index.js
+++ latest/search_index.js
@@ -68,7 +68,7 @@ var documenterSearchIndex = {"docs": [
     "location": "ma.html#MarketTechnicals.sma",
     "page": "Moving Averages",
     "title": "MarketTechnicals.sma",
-    "category": "Function",
+    "category": "function",
     "text": "sma(arr, n)\n\nSimple Moving Average\n\nSMA = fracsum_i^nP_in\n\n\n\n"
 },
 
@@ -84,7 +84,7 @@ var documenterSearchIndex = {"docs": [
     "location": "ma.html#MarketTechnicals.ema",
     "page": "Moving Averages",
     "title": "MarketTechnicals.ema",
-    "category": "Function",
+    "category": "function",
     "text": "ema(arr, n, wilder=false)\n\nExponemtial Moving Average\n\nA.k.a. exponentially weighted moving average (EWMA)\n\n    textLet  k textdenote the degree of weighting decrease\n\nIf parameter wilder is true, k = frac1n, else k = frac2n + 1.\n\n    EMA_t = k times P_t + (1 - k) times EMA_t - 1\n\n\n\n"
 },
 
@@ -100,14 +100,14 @@ var documenterSearchIndex = {"docs": [
     "location": "ma.html#MarketTechnicals.kama",
     "page": "Moving Averages",
     "title": "MarketTechnicals.kama",
-    "category": "Function",
-    "text": "Kaufman's Adaptive Moving Average\n\nArguments:\n\nn: period\nfn: the fastest EMA constant\nsn: the slowest EMA constant\n\nFormula:\n\n    beginalign*\n        KAMA_t  = KAMA_t-1 + SC times (Price - KAMA_t-1) \n        SC      =\n            (ER times (frac2fn + 1 - frac2sn + 1) + frac2sn + 1)^2 \n        ER      = fracChangeVolatility \n        Change  =  Price - Price_t-n  \n        Volatility  = sum_i^n  Price_i - Price_i-1 \n    endalign*\n\n\n\n"
+    "category": "function",
+    "text": "Kaufman\'s Adaptive Moving Average\n\nArguments:\n\nn: period\nfn: the fastest EMA constant\nsn: the slowest EMA constant\n\nFormula:\n\n    beginalign*\n        KAMA_t  = KAMA_t-1 + SC times (Price - KAMA_t-1) \n        SC      =\n            (ER times (frac2fn + 1 - frac2sn + 1) + frac2sn + 1)^2 \n        ER      = fracChangeVolatility \n        Change  =  Price - Price_t-n  \n        Volatility  = sum_i^n  Price_i - Price_i-1 \n    endalign*\n\n\n\n"
 },
 
 {
-    "location": "ma.html#Kaufman's-Adaptive-Moving-Average-1",
+    "location": "ma.html#Kaufman\'s-Adaptive-Moving-Average-1",
     "page": "Moving Averages",
-    "title": "Kaufman's Adaptive Moving Average",
+    "title": "Kaufman\'s Adaptive Moving Average",
     "category": "section",
     "text": "kamausing MarketData\nusing MarketTechnicals\n\nkama(cl)"
 },
@@ -132,7 +132,7 @@ var documenterSearchIndex = {"docs": [
     "location": "levels.html#MarketTechnicals.floorpivots",
     "page": "Trade levels",
     "title": "MarketTechnicals.floorpivots",
-    "category": "Function",
+    "category": "function",
     "text": "floorpivots(ohlc)\n\nFloor Trader Pivots\n\nbeginalign*\n\n    R3  = Pivot_t + (R2 - S1) \n    R2  = Pivot_t + (R1 - S1) \n    R1  = 2 Pivot_t - P^low_t-1 \n    Pivot_t  = Price^typical_t-1 =\n        fracP^high_t-1 + P^low_t-1 + P^close_t-13 \n    S1  = 2 Pivot_t - P^high_t-1 \n    S2  = Pivot_t - (R1 - S1) \n    S3  = Pivot_t - (R2 - S1)\n\nendalign*\n\n\n\n"
 },
 
@@ -148,8 +148,8 @@ var documenterSearchIndex = {"docs": [
     "location": "levels.html#MarketTechnicals.woodiespivots",
     "page": "Trade levels",
     "title": "MarketTechnicals.woodiespivots",
-    "category": "Function",
-    "text": "woodiespivots(ohlc)\n\nWoodie's Pivot\n\nbeginalign*\n\n    Range  = Price^high_t-1 - Price^low_t-1 \n\n    R4  = S4 + Range tagnot implemented \n    R3  = R1 + Range \n    R2  = Pivot_t + Range \n    R1  = 2 Pivot_t - Price^low_t-1 \n    Pivot_t  =\n        fracPrice^high_t-1 + Price^low_t-1 + 2 Price^open_t4 \n    S1  = 2 Pivot_t - Price^high_t-1 \n    S2  = Pivot_t - Range \n    S3  = S1 - Range \n    S4  = S3 - Range tagnot implemented\n\nendalign*\n\n\n\n"
+    "category": "function",
+    "text": "woodiespivots(ohlc)\n\nWoodie\'s Pivot\n\nbeginalign*\n\n    Range  = Price^high_t-1 - Price^low_t-1 \n\n    R4  = S4 + Range tagnot implemented \n    R3  = R1 + Range \n    R2  = Pivot_t + Range \n    R1  = 2 Pivot_t - Price^low_t-1 \n    Pivot_t  =\n        fracPrice^high_t-1 + Price^low_t-1 + 2 Price^open_t4 \n    S1  = 2 Pivot_t - Price^high_t-1 \n    S2  = Pivot_t - Range \n    S3  = S1 - Range \n    S4  = S3 - Range tagnot implemented\n\nendalign*\n\n\n\n"
 },
 
 {
@@ -180,7 +180,7 @@ var documenterSearchIndex = {"docs": [
     "location": "momentum.html#MarketTechnicals.adx",
     "page": "Momentum",
     "title": "MarketTechnicals.adx",
-    "category": "Function",
+    "category": "function",
     "text": "adx(ohlc, n=14; h=\"High\", l=\"Low\", c=\"Close\")\n\nAverage Directional Movement Index\n\nDeveloped by J. Welles Wilder. This Implementation follow StockCharts.\n\nReference\n\nwikipedia\nStockCharts\n\n\n\n"
 },
 
@@ -196,7 +196,7 @@ var documenterSearchIndex = {"docs": [
     "location": "momentum.html#MarketTechnicals.aroon",
     "page": "Momentum",
     "title": "MarketTechnicals.aroon",
-    "category": "Function",
+    "category": "function",
     "text": "aroon(ohlc, n=25; h=\"High\", l=\"Low\")\n\nAroon Oscillator\n\nFormula\n\n    beginalign*\n        up    = fracmathopargmax(High_t-n dots High_t)n times 100 \n        down  = fracmathopargmin(Low_t-n dots Low_t)n times 100 \n        osc   = up - down\n    endalign*\n\nReference\n\nStockCharts\n\n\n\n"
 },
 
@@ -212,7 +212,7 @@ var documenterSearchIndex = {"docs": [
     "location": "momentum.html#MarketTechnicals.rsi",
     "page": "Momentum",
     "title": "MarketTechnicals.rsi",
-    "category": "Function",
+    "category": "function",
     "text": "rsi(ta, n=14; wilder=false)\n\nRelative Strength Index\n\n    RSI = fracEMA(Up n)EMA(Up n) + EMA(Dn n)\n\n\n\n"
 },
 
@@ -228,7 +228,7 @@ var documenterSearchIndex = {"docs": [
     "location": "momentum.html#MarketTechnicals.macd",
     "page": "Momentum",
     "title": "MarketTechnicals.macd",
-    "category": "Function",
+    "category": "function",
     "text": "macd(ta, fast=12, slow=26, signal=9; wilder=false)\n\nMoving Average Convergence / Divergence\n\n    beginalign*\n        MACD Bar  = DIF - DEM \n        DIF  = EMA(P_close fast) - EMA(P_close slow) \n        DEM  = EMA(DIF 9) tagsignal\n    endalign*\n\nReturn:\n\nTimeArray with 3 columns [\"macd\", \"dif\", \"signal\"].\n\nIf the input is a multi-column TimeArray, the new column names will be [\"A_macd\", \"B_macd\", \"A_dif\", \"B_dif\", \"A_signal\", \"B_signal\"].\n\n\n\n"
 },
 
@@ -244,7 +244,7 @@ var documenterSearchIndex = {"docs": [
     "location": "momentum.html#MarketTechnicals.chaikinoscillator",
     "page": "Momentum",
     "title": "MarketTechnicals.chaikinoscillator",
-    "category": "Function",
+    "category": "function",
     "text": "chaikinoscillator(ohlcv, fast=3, slow=10; h=\"High\", l=\"Low\", c=\"Close\")\n\nChaikin Oscillator\n\nDeveloped by Marc Chaikin\n\nFormula\n\n    Chaikin OSC = EMA(ADL fast) - EMA(ADL slow)\n\nwhere the adl is the Accumulation/Distribution Line.\n\nReference\n\nStockCharts\n\n\n\n"
 },
 
@@ -260,7 +260,7 @@ var documenterSearchIndex = {"docs": [
     "location": "momentum.html#MarketTechnicals.cci",
     "page": "Momentum",
     "title": "MarketTechnicals.cci",
-    "category": "Function",
+    "category": "function",
     "text": "cci(ohlc, ma=20, c=0.015)\n\nCommodity Channel Index\n\n    CCI = fracP_typical - SMA(P_typical)c times sigma(P_typical)\n\nReference\n\nhttps://en.wikipedia.org/wiki/Commodity_channel_index\n\n\n\n"
 },
 
@@ -276,7 +276,7 @@ var documenterSearchIndex = {"docs": [
     "location": "momentum.html#MarketTechnicals.roc",
     "page": "Momentum",
     "title": "MarketTechnicals.roc",
-    "category": "Function",
+    "category": "function",
     "text": "roc(ta, n)\n\nRate of Change\n\nFormula:\n\n    roc = fracclose_t - close_t-nclose_t-n\n\nReference:\n\nWikipedia\n\n\n\n"
 },
 
@@ -292,7 +292,7 @@ var documenterSearchIndex = {"docs": [
     "location": "momentum.html#MarketTechnicals.stochasticoscillator",
     "page": "Momentum",
     "title": "MarketTechnicals.stochasticoscillator",
-    "category": "Function",
+    "category": "function",
     "text": "stochasticoscillator(ohlc, n=14, fast_d=3, slow_d=3; h=\"High\", l=\"Low\", c=\"Close\")\n\nStochastic Oscillator\n\nA.k.a %K%D, or KD\n\nParameter\n\nn: period of fast(raw) %K\nfast_d: MA period of fast %D\nslow_d: MA period of slow %D\n\nFormula\n\n    beginalign*\n        fast K  = fracClose_t - max(High_t-n dots High_t)\n            max(High_t-n dots High_t) - min(Low_t-n dots Low_t)\n            times 100 \n        fast D  = SMA(fast K) \n        slow D  = SMA(fast D)\n    endalign*\n\nReference\n\nWikipedia\nFMLabs\n\n\n\n"
 },
 
@@ -324,7 +324,7 @@ var documenterSearchIndex = {"docs": [
     "location": "volatility.html#MarketTechnicals.bollingerbands",
     "page": "Volatility",
     "title": "MarketTechnicals.bollingerbands",
-    "category": "Function",
+    "category": "function",
     "text": "bollingerbands(ta, ma=20, width=2.0)\n\nBollinger Bands\n\nbeginalign*\n\n    Up  = SMA + width times sigma \n    Mean  = SMA \n    Down  = SMA - width times sigma\n\nendalign*\n\n\n\n"
 },
 
@@ -340,7 +340,7 @@ var documenterSearchIndex = {"docs": [
     "location": "volatility.html#MarketTechnicals.chaikinvolatility",
     "page": "Volatility",
     "title": "MarketTechnicals.chaikinvolatility",
-    "category": "Function",
+    "category": "function",
     "text": "chaikinvolatility(ta, n=10, p=10; h=\"High\", l=\"Low\")\n\nChaikin Volatility\n\nParameters\n\nn is the smooth period\np is the previous period\n\nFormula\n\n    Chaikin Vola =\n        fracEMA(High_t - Low_t n) - EMA(High_t-p - Low_t-p n)\n        EMA(High_t-p - Low_t-p n)\n        times 100\n\nReference\n\nIncredibleCharts\n\n\n\n"
 },
 
@@ -356,7 +356,7 @@ var documenterSearchIndex = {"docs": [
     "location": "volatility.html#MarketTechnicals.truerange",
     "page": "Volatility",
     "title": "MarketTechnicals.truerange",
-    "category": "Function",
+    "category": "function",
     "text": "truerange(ohlc; h=\"High\", l=\"Low\", c=\"Close\")\n\nTrue Range\n\n    TR = max (H_t C_t-1) - min (L_t Ct-1)\n\n\n\n"
 },
 
@@ -372,8 +372,8 @@ var documenterSearchIndex = {"docs": [
     "location": "volatility.html#MarketTechnicals.atr",
     "page": "Volatility",
     "title": "MarketTechnicals.atr",
-    "category": "Function",
-    "text": "atr(ohlc, n=14; h=\"High\", l=\"Low\", c=\"Close\")\n\nAverage True Range\n\nIt's the exponential moving average of truerange\n\n    ATR = EMA(TR n)\n\n\n\n"
+    "category": "function",
+    "text": "atr(ohlc, n=14; h=\"High\", l=\"Low\", c=\"Close\")\n\nAverage True Range\n\nIt\'s the exponential moving average of truerange\n\n    ATR = EMA(TR n)\n\n\n\n"
 },
 
 {
@@ -388,7 +388,7 @@ var documenterSearchIndex = {"docs": [
     "location": "volatility.html#MarketTechnicals.donchianchannels",
     "page": "Volatility",
     "title": "MarketTechnicals.donchianchannels",
-    "category": "Function",
+    "category": "function",
     "text": "donchianchannels(ta, n=20; h=\"High\", l=\"Low\")\n\nDonchian Channels\n\nFormula\n\n    beginalign*\n        Up    = max (High_1 to High_t) \n        Mid   = fracUp + Down2 \n        Down  = min (Low_1 to Low_t)\n    endalign*\n\nReference\n\nTradingView Wiki\n\n\n\n"
 },
 
@@ -404,7 +404,7 @@ var documenterSearchIndex = {"docs": [
     "location": "volatility.html#MarketTechnicals.keltnerbands",
     "page": "Volatility",
     "title": "MarketTechnicals.keltnerbands",
-    "category": "Function",
+    "category": "function",
     "text": "keltnerbands(ohlc, n=20, w=2; h=\"High\", l=\"Low\", c=\"Close\")\n\nKeltner Channels\n\nLinda Bradford Raschke introduced the newer version of Keltner Channels in the 1980s. We implement the newer version.\n\nFormula\n\n    beginalign*\n        textUp    = textMid + w times ATR(n) \n        textMid   = EMA(P_typical n) \n        textDown  = textMid - w times ATR(n)\n    endalign*\n\nReference\n\nStockCharts\nWikipedia\n\n\n\n"
 },
 
@@ -436,7 +436,7 @@ var documenterSearchIndex = {"docs": [
     "location": "volume.html#MarketTechnicals.adl",
     "page": "Volume",
     "title": "MarketTechnicals.adl",
-    "category": "Function",
+    "category": "function",
     "text": "adl(ohlcv; h=\"High\", l=\"Low\", c=\"Close\", v=\"Volume\")\n\nAccumulation/Distribution Line\n\nDeveloped by Marc Chaikin.\n\nFormula\n\n    ADL_t = ADL_t-1 +\n        frac(Close_t - Low_t) - (High_t - Close_t)High_t - Low_t\n        times Volume_t\n\nReference\n\nStockCharts\n\n\n\n"
 },
 
@@ -452,7 +452,7 @@ var documenterSearchIndex = {"docs": [
     "location": "volume.html#MarketTechnicals.obv",
     "page": "Volume",
     "title": "MarketTechnicals.obv",
-    "category": "Function",
+    "category": "function",
     "text": "obv(ohlcv; price=\"Close\", v=\"Volume\")\n\nOn Balance Volume\n\n    OBV_t = OBV_t - 1 +\n        begincases\n            volume   textif  close_t  close_t-1 \n            0        textif  close_t = close_t-1 \n            -volume  textif  close_t  close_t-1\n        endcases\n\n\n\n"
 },
 
@@ -468,7 +468,7 @@ var documenterSearchIndex = {"docs": [
     "location": "volume.html#MarketTechnicals.vwap",
     "page": "Volume",
     "title": "MarketTechnicals.vwap",
-    "category": "Function",
+    "category": "function",
     "text": "vwap(ohlcv, n; price=\"Close\", v=\"Volume\")\n\nVolume Weight-Adjusted Price\n\n    P = fracsum_j P_j Q_jsum_j Q_j  textwhere Q is the volume\n\n\n\n"
 },
 
@@ -493,7 +493,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Candlesticks",
     "title": "Candlesticks",
     "category": "section",
-    "text": "Candlestick depiction of price involves three components: open, high, low and close. The body of the candlestick consists of the open and close, and the color of the candle body depicts which one is on top and which is on the bottom. Green is typically reserved for a higher close than the open, because traders were able to realize profits for the day. Red depicts a higher open and a lower close.The candlestick also includes wicks to the top and the bottom. These wicks are bounded by the high for the top wick and low for the bottom wick.Candlestick patterns identify certain formations for these candlesticks. Sometimes these patterns only concern a single trading day's activity and other times it spans several trading periods."
+    "text": "Candlestick depiction of price involves three components: open, high, low and close. The body of the candlestick consists of the open and close, and the color of the candle body depicts which one is on top and which is on the bottom. Green is typically reserved for a higher close than the open, because traders were able to realize profits for the day. Red depicts a higher open and a lower close.The candlestick also includes wicks to the top and the bottom. These wicks are bounded by the high for the top wick and low for the bottom wick.Candlestick patterns identify certain formations for these candlesticks. Sometimes these patterns only concern a single trading day\'s activity and other times it spans several trading periods."
 },
 
 {
@@ -524,7 +524,7 @@ var documenterSearchIndex = {"docs": [
     "location": "utils.html#MarketTechnicals.typical",
     "page": "Utilities",
     "title": "MarketTechnicals.typical",
-    "category": "Function",
+    "category": "function",
     "text": "typical(ohlc; h=\"High\", l=\"Low\", c=\"Close\")\n\nTypical Price\n\n    textTypical Price = fracH + L + C3\n\n\n\n"
 },
 
