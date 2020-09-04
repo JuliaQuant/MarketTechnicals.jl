b8ac4a822a6bf4d779894e1ea06b40a5736e24de
Iblis Lin <iblis@hs.ntnu.edu.tw>
2020-09-04 13:07:10 +0800

    indent

--- docs/make.jl
+++ docs/make.jl
@@ -3,24 +3,24 @@ using MarketTechnicals
 
 
 makedocs(
-    format = Documenter.HTML(mathengine = Documenter.Writers.HTMLWriter.MathJax3()),
-    sitename = "MarketTechnicals.jl",
-    modules = [MarketTechnicals],
-    pages = [
-        "index.md",
-        "getting_started.md",
-        "ma.md",
-        "levels.md",
-        "momentum.md",
-        "volatility.md",
-        "volume.md",
-        "candlesticks.md",
-        "utils.md",
-    ]
+  format = Documenter.HTML(mathengine = Documenter.Writers.HTMLWriter.MathJax3()),
+  sitename = "MarketTechnicals.jl",
+  modules = [MarketTechnicals],
+  pages = [
+    "index.md",
+    "getting_started.md",
+    "ma.md",
+    "levels.md",
+    "momentum.md",
+    "volatility.md",
+    "volume.md",
+    "candlesticks.md",
+    "utils.md",
+  ],
 )
 
 deploydocs(
-    repo         = "github.com/JuliaQuant/MarketTechnicals.jl.git",
-    devbranch    = "master",
-    push_preview = true,
+  repo         = "github.com/JuliaQuant/MarketTechnicals.jl.git",
+  devbranch    = "master",
+  push_preview = true,
 )
